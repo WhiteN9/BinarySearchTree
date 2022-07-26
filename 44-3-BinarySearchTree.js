0448c2d4186c025107c61278b8c235c8cdbe1a7e
White Nguyen <whitenguyen0909@gmail.com>
2022-07-26 08:46:05 -0600

    44-3 binary search tree remove node

--- 44-3-BinarySearchTree.js
+++ 44-3-BinarySearchTree.js
@@ -62,6 +62,115 @@ class BinarySearchTree {
       throw new Error("Key Not Found");
     }
   }
+
+  remove(key) {
+    // If we found the node's key to be removed.
+    // Remove nodes based on how many children the parent has
+    if (this.key == key) {
+      //If the node has 2 children
+      if (this.left && this.right) {
+        // Replace the key and value of the node to be removed.
+        // Search for the successor node.
+        // MAXIMUM on the left subtree or MINIMUM on the right subtree.
+
+        // _findMin() is a helper to find the smallest node but still greater than the targeted node.
+        // We go to the right one time, and find the ABSOLUTE minimum node by recursively going left after.
+        const successor = this.right._findMin();
+        // We can use a _findMax() when we try to replace a node with a lesser value.
+        // const successor = this.left._findMax();
+
+        // Replace the key and value of the node to be removed.
+        this.key = successor.key;
+        this.value = successor.value;
+
+        // Remove the old (duplicate) node from the BST
+        successor.remove(successor.key); // Successor is a leaf node, so it will get instantly removed.
+      }
+      // Else if, the node only has a left child, then you replace the node with its left child.
+      else if (this.left) {
+        // Replace current node with its left child
+        this._replaceWith(this.left);
+      }
+      // Else if, the node only has a right child, then you place it with its right child.
+      else if (this.right) {
+        // Replace current node with its right child
+        this._replaceWith(this.right);
+      }
+      // Else if, node has no children
+      else {
+        this.replaceWith(null);
+      }
+    }
+    // Else if, we have not found the node with that key., we keep traversing down left/right recursively to find it
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    // Else if, the node with that key does not exist, throw an Error.
+    else {
+      throw new Error("Key Not Found");
+    }
+  }
+
+  // Replace the current node with the node that is passed in
+  _replaceWith(node) {
+    // Check if the node we are placing has a parent
+    // Connect the references from the parent to the replacement node
+    if (this.parent) {
+      // If the current node to-be-replaced is on the left of parent node
+      if (this == this.parent.left) {
+        // We point the parent node's left to the passed in replacement node.
+        //Cutting ties with the current node
+        this.parent.left = node;
+      }
+      // If the current node to-be-replaced is on the right of parent node
+      else if (this == this.parent.right) {
+        // We point the parent node's right to the passed in replacement node.
+        this.parent.right = node;
+      }
+
+      //Then, we replace the replacement node's parent reference
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    // Else if, the current node is a root node, we copy over the properties of the replacement node.
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      // If "null" was passed in.
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    //If the current node has no left child node, it is the ABSOLUTE smallest
+    if (!this.left) {
+      return this;
+    }
+    // recursively traverse to the left
+
+    return this.left._findMin();
+  }
+
+  _findMax() {
+    //If the current node has no right child node, it is the ABSOLUTE largest
+    if (!this.right) {
+      return this;
+    }
+    // recursively traverse to the right
+    return this.right.findMax();
+  }
 }
 
 module.exports = BinarySearchTree;
