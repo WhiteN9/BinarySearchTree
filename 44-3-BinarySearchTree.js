3f4c330a5aad4abc877a9086e3de77665c26ee9c
White Nguyen <whitenguyen0909@gmail.com>
2022-07-25 18:45:17 -0600

    added comments for inserting node

--- 44-3-BinarySearchTree.js
+++ 44-3-BinarySearchTree.js
@@ -8,23 +8,32 @@ class BinarySearchTree {
   }
 
   insert(key, value) {
-    //if the key is null, there is no node and the tree is empty
+    //If the key is null, there is no node and the tree is empty.
+    //We insert a node into the tree as root node.
     if (this.key == null) {
-      //we insert a node into the tree as root node
       this.key = key;
       this.value = value;
-      //if the key we want to insert is less than the existing node key
+      //Else if the tree exists, and the key we want to insert is less than the existing current node's key, we insert to the left.
     } else if (key < this.key) {
-      //
+      //If the left node is empty, we insert a left tree.
       if (this.left == null) {
-        this.left = new BinarySearchTree(key, value, this);
-      } else {
+        this.left = new BinarySearchTree(key, value, this); // `this` refers to the current node
+      }
+      //Else, there is an existing left child, then
+      //We recursively call the insert() method so the node is added further down
+      else {
         this.left.insert(key, value);
       }
-    } else {
+    }
+    //We do the same thing for the right side, if the key we want to insert is more than the existing current node's key.
+    else {
+      //If the right node is empty, we insert a right tree.
       if (this.right == null) {
-        this.right = new BinarySearchTree(key, value, this);
-      } else {
+        this.right = new BinarySearchTree(key, value, this); // `this` refers to the current node
+      }
+      //Else, there is an existing right child, then
+      //We cursively call the insert() method so the node is added further down
+      else {
         this.right.insert(key, value);
       }
     }
